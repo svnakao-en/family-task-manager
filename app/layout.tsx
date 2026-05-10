8bb3893e68210f15f7caa42e9ea70465b6741750
y-nakao <y-nakao@grand-design.jp>
2026-05-10 09:49:39 +0900

    TEST：layout.tsxを少し戻す

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,18 @@
 // app/layout.tsx
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="ja">
-      <body style={{ margin: 0, padding: 0, backgroundColor: 'white' }}>
+      <body className={inter.className} style={{ margin: 0, padding: 0 }}>
+        {/* 今はRoleGuardを入れず、中身だけを素直に出す設定にします */}
         {children}
       </body>
     </html>
-  )
+  );
 }
